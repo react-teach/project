eac925b573192c9c513a2ca853f0656a4aa100ee
Melike Öztekin <melikeoztekin06@gmail.com>
2022-08-28 22:12:43 +0300

    Create showcase

--- src/components/Cards/IconCard.jsx
+++ src/components/Cards/IconCard.jsx
@@ -6,26 +6,41 @@ import styled from "styled-components";
 function IconCard() {
   return (
     <Div>
-      <div align="center">
-        <A className="icon">
-          <AvTimerIcon className="faIcon" />
-        </A>
-        <B>
-          <Typography variant="h4" component="h2" my={2}>
-            Sıfırlanmayan Dakikalar
-          </Typography>
-          <Typography variant="body1" fontSize={16}>
-            Aldığın dakikaların son kullanma tarihi yok. Dakikaların bitene
-            kadar senin, istediğin zaman kullanabilirsin!
-          </Typography>
-        </B>
+      <div className="icon">
+        <AvTimerIcon className="faIcon" />
+      </div>
+      <div className="content">
+        <Typography className="title" variant="h5" component="h2" mb={3}>
+          Sıfırlanmayan Dakikalar
+        </Typography>
+        <Typography variant="body1" fontSize={16}>
+          Aldığın dakikaların son kullanma tarihi yok. Dakikaların bitene kadar
+          senin, istediğin zaman kullanabilirsin!
+        </Typography>
       </div>
     </Div>
   );
 }
 
 const Div = styled.div`
-  padding: 0;
-  flex-direction: row;
+  display: flex;
+  align-items: flex-start;
+  padding-bottom: 50px;
+  .faIcon {
+    font-size: 64px;
+    line-height: 56px;
+    color: #2aa275;
+    margin-right: 15px;
+  }
+  .title {
+    font-size: 24px;
+    font-weight: 500;
+    transition: 0.4s;
+  }
+  :hover {
+    .title {
+      color: #2aa275;
+    }
+  }
 `;
 export default IconCard;
--- src/components/showcase/Showcase.jsx
+++ src/components/showcase/Showcase.jsx
@@ -36,16 +36,48 @@ function Showcase() {
       <h1>styled-component ile yazıldı</h1>
       <Grid
         container
-        spacing={5}
-        sx={{ px: 12, display: "flex", flexDirection: "column" }}
+        sx={{
+          px: 12,
+          py: 12,
+          display: "flex",
+          alignItems: "center",
+          flexDirection: "row",
+          backgroundColor: "#f1f1f1",
+        }}
       >
-        <Grid item xs={12} md={4} lg={4}>
+        <Grid
+          item
+          xs={12}
+          md={6}
+          lg={4}
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+          }}
+        >
+          <IconCard></IconCard>
+          <IconCard></IconCard>
           <IconCard></IconCard>
         </Grid>
-        <Grid item xs={12} md={4} lg={4}>
-          <IconCard></IconCard>
+        <Grid item lg={4}>
+          <img
+            width={400}
+            src="https://sorucevap.inekle.com/images/inekle-app-ozellikler.png"
+            loading="lazy"
+          />
         </Grid>
-        <Grid item xs={12} md={4} lg={4}>
+        <Grid
+          item
+          xs={12}
+          md={6}
+          lg={4}
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+          }}
+        >
+          <IconCard></IconCard>
+          <IconCard></IconCard>
           <IconCard></IconCard>
         </Grid>
       </Grid>
