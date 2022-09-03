14db661d86a83f271add2225b3394b5157cc4e23
nacarkahya25 <mehmetemin.nacarkahya@gmail.com>
2022-09-03 18:27:42 +0300

    Navbar yapıldı

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,19 +1,4 @@
-<<<<<<< HEAD
-import React from 'react'
-import AppButton from '../Buttons/AppButton'
 
-function Header() {
-  return (
-<<<<<<< HEAD
-    <div>Headerdsds</div>
-=======
-    <div>
-      <h1>
-      Header component</h1>
-      <AppButton/>
-      </div>
->>>>>>> origin/melikeoztekin
-=======
 import { useMediaQuery } from "@mui/material";
 import { useTheme } from "@mui/material/styles";
 import {ListItemText, ListItemButton, ListItemIcon} from "@mui/material";
@@ -44,7 +29,6 @@ export default function Header(){
       {matches ? <AppbarContainer/> : <AppbarHeader/>}
     
     </>
->>>>>>> c8a8ae34996a740dfb3b59ee6dc01deb22203221
   )
 
 
